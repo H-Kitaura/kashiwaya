25aeda9a3c20a7a09fd35631898066156fbb4a79
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2023-01-04 21:59:29 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,6 @@
 
  <x-tests.card title="タイトル" content="本文1" :message="$message"></x-tests.card>
  <x-tests.card title="タイトル2" />
+ <x-tests.card title="cssを変更したい" class="bg-red-300" />
 </x-tests.app>
 
