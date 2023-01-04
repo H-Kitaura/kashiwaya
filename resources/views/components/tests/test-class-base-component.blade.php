2f1fec09e2def8929839099a349bbfb59e0bf0a4
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2023-01-04 22:17:09 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+</div>
