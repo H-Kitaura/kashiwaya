60876684874170c6243311da754d26f2c599c095
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2023-01-16 16:02:02 +0900

    エラー文、日本語対応

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Owner; //Eloquent エロクエント
 use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダー
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
 
 
 class OwnersController extends Controller
@@ -66,7 +68,20 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->name;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.Owner::class],
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+return redirect()->route('admin.owners.index');
     }
 
     /**
